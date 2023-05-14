43c9c26c664a7e3fb7f1157d8645fc0390720706
lijo-santhosh <109999642+lijo-santhosh@users.noreply.github.com>
2023-05-14 20:25:00 +0930

    Update ArrMin.asm

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -2,15 +2,10 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-
-
-// Initialize index and smallest to 0
 @i
 M=0
 @smallest
 M=0
-
-// Set R0 to the first element of R1
 @R1
 A=M
 D=M
@@ -18,32 +13,24 @@ D=M
 M=D
 
 (LOOP)
-// Loop until i equals R2
+
 @i
 D=M
 @R2
 D=M-D
 @END
 D;JEQ
-
-// Access element
 @R1
 D=M
 @i
 A=D+M
 D=M
-
-// Check if less than smallest
 @R0
 D=D-M
 @LESS
 D;JLT
-
-// If false, continue to next iteration
 @CONTINUE
 0;JMP
-
-// If true, update smallest
 (LESS)
 @R1
 D=M
@@ -54,11 +41,9 @@ D=M
 M=D
 
 (CONTINUE)
-// Increment index
+
 @i
 M=M+1
-
-// Repeat loop
 @LOOP
 0;JMP
 
